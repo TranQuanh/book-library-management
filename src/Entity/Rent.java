8f101bc51986868ee2371e6568407ec3f1bac62f
Nguyenphuc-dev <nguyenphuc210804@gmail.com>
2024-11-13 10:21:38 +0700

    create Rent

---
+++ src/Entity/Rent.java
@@ -0,0 +1,83 @@
+package Entity;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class Rent {
+    private int ID;
+    private User user;
+    private Book book;
+    private LocalDateTime borrowTime;
+    private LocalDateTime endTime;
+    private int totalDays;
+    private boolean status;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+
+    public Rent(){
+        borrowTime = LocalDateTime.now();
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDateTime getBorrowTime() {
+        return borrowTime;
+    }
+
+    public void setBorrowTime(LocalDateTime borrowTime) {
+        this.borrowTime = borrowTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getTotalDays() {
+        return totalDays;
+    }
+
+    public void setTotalDays(int totalDays) {
+        this.totalDays = totalDays;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public DateTimeFormatter getFormatter() {
+        return formatter;
+    }
+
+    public void setFormatter(DateTimeFormatter formatter) {
+        this.formatter = formatter;
+    }
+
+}
