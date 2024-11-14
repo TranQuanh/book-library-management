318fb0d6c072ebfab834bb8f943be35a7681819b
Nguyenphuc-dev <nguyenphuc210804@gmail.com>
2024-11-14 11:17:35 +0700

    codeClient

---
+++ src/Entity/Client.java
@@ -0,0 +1,15 @@
+package Entity;
+
+public class Client extends User{
+    public Client() {
+        super();
+    }
+    public void showList(){
+        System.out.println("\n1. View Books");
+        System.out.println("2. Rent Book");
+        System.out.println("3. Return Book");
+        System.out.println("4. Show My Rents");
+        System.out.println("5. Edit My Data");
+        System.out.println("6. Quit\n");
+    }
+}
