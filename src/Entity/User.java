191c56f3ef4b930ede43afbebb979a5c30d33362
Nguyenphuc-dev <nguyenphuc210804@gmail.com>
2024-11-14 10:15:14 +0700

    create User

---
+++ src/Entity/User.java
@@ -0,0 +1,69 @@
+package Entity;
+
+public abstract class User {
+    private String ID;
+    private String name;
+    private String email;
+    private String userName;
+    private String password;
+    private String phoneNumber;
+    private String location;
+
+    public User() {}
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    public abstract void showList();
+}
