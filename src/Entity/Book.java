5909e9793a1bada796ef038edb4714bf9d2dde2f
quang anh <pewpewls09@gmail.com>
2024-11-14 09:37:06 +0700

    code Book

---
+++ src/Entity/Book.java
@@ -0,0 +1,50 @@
+package Entity;
+
+public class Book {
+    private String ID;
+    private String name;
+    private String author;
+    private String publisher;
+    private int count;
+    public Book(){}
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
