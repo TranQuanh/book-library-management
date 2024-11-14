9c902d873fb11d8a91322ce4c09cdb0b430a57ef
quang anh <pewpewls09@gmail.com>
2024-11-14 11:30:59 +0700

    add Database

---
+++ src/Entity/Database.java
@@ -0,0 +1,22 @@
+package Entity;
+
+import java.sql.*;
+
+public class Database {
+    private String user ="root";
+    private String password =  "111111";
+    private String url = "jdbc:mysql://localhost:3306/bookmanagement";
+    Statement statement;
+
+    public Database() {
+        try{
+            Connection connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public Statement getStatement() {
+        return statement;
+    }
+}
