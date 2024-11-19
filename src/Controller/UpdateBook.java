1fd3c0b819049848b56aeb030a4daf84351bad32
Nguyenphuc-dev <nguyenphuc210804@gmail.com>
2024-11-19 20:31:38 +0700

    code

--- src/Controller/UpdateBook.java
+++ src/Controller/UpdateBook.java
@@ -6,10 +6,11 @@ import Model.Operation;
 import Model.User;
 
 public class UpdateBook implements Operation {
-    public void operation()
 
     @Override
     public void operation(Database database, Scanner sc, User user) {
-
+        System.out.println("Enter book ID(int):(-1 to show all books)");
+        int ID = sc.nextInt();
+        String update = "UPDATE cars SET Name = '[value-2]', Author = '[value-3]', Publisher = '[value-4]', Count = '[value-5]', Price = '[value-6]', Available = '[value-7]' WHERE ID = '';";
     }
 }
