7ca249d469c4c23f53f738a68dbe7070931a7093
Son Phi <phiquoctuson2004@gmail.com>
2024-11-26 23:52:08 +0700

    Update code Gui viewbook & updatebook & addnewbook

--- src/Controller/AddNewBook.java
+++ src/Controller/AddNewBook.java
@@ -1,36 +1,108 @@
-//package Controller;
-//import java.sql.ResultSet;
-//import java.sql.SQLOutput;
-//import java.util.*;
-//import java.sql.SQLException;
-//import Model.Database;
-//import Model.Operation;
-//import Model.User;
-//
-//public class AddNewBook implements Operation {
-//    @Override
-//    public void operation(Database database, Scanner sc, User user){
-//        System.out.println("Enter name:");
-//        String name = sc.next();
-//        System.out.println("Enter author:");
-//        String author = sc.next();
-//        System.out.println("Enter publisher:");
-//        String publisher = sc.next();
-//        System.out.println("Enter number of books");
-//        int count = sc.nextInt();
-//        try {
-//            ResultSet rs = database.getStatement().executeQuery("SELECT COUNT(*) as count FROM `book`");
-//            rs.next();
-//            int ID = rs.getInt("count") + 1;
-//            String insert = "INSERT INTO `book`(`id`,`name`,`author`, " +
-//                    " `publisher`,`count`) VALUES " +
-//                    " ('"+ID+"', '"+name+"', '"+author+"', '"+publisher+"'," +
-//                    "'"+count+"');";
-//            database.getStatement().executeUpdate(insert);
-//            System.out.println("Book added successfully");
-//        }
-//        catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package Controller;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.sql.SQLException;
+import Model.Database;
+import Model.JButton;
+import Model.JLabel;
+import Model.JTextField;
+import Model.Operation;
+import Model.User;
+
+import javax.swing.*;
+
+public class AddNewBook implements Operation {
+    @Override
+    public void operation(Database database, JFrame f, User user){
+        JFrame frame = new JFrame("Add New Book");
+        frame.setSize(600,525);
+        frame.setLocationRelativeTo(f);
+        frame.getContentPane().setBackground(new Color(27, 150, 250));
+        frame.setLayout(new BorderLayout());
+
+        Model.JLabel title = new JLabel("Add New Book",35);
+        title.setBorder(BorderFactory.createEmptyBorder(20,0,0,0));
+        frame.add(title, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel(new GridLayout(6,2,15,15));
+        panel.setBackground(null);
+        panel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+
+        panel.add(new JLabel("Name:",22));
+        JTextField name = new JTextField(22);
+        panel.add(name);
+
+        panel.add(new JLabel("Author:",22));
+        JTextField author = new JTextField(22);
+        panel.add(author);
+
+        panel.add(new JLabel("Publisher:",22));
+        JTextField publisher = new JTextField(22);
+        panel.add(publisher);
+
+        panel.add(new JLabel("Number of books:",22));
+        JTextField number = new JTextField(22);
+        panel.add(number);
+
+        JButton cancel = new JButton("Cancel",22);
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+
+        });
+        panel.add(cancel);
+
+        JButton save = new JButton("Save",22);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (name.getText().equals("")) {
+                    JOptionPane.showMessageDialog(frame, "Please enter a title");
+                }
+                if (author.getText().equals("")) {
+                    JOptionPane.showMessageDialog(frame, "Please enter a author");
+                }
+                if (publisher.getText().equals("")) {
+                    JOptionPane.showMessageDialog(frame, "Please enter a publisher");
+                }
+                if (number.getText().equals("")) {
+                    JOptionPane.showMessageDialog(frame, "Please enter a number");
+                }
+                int num ;
+                try {
+                    num = Integer.parseInt(number.getText());
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Please enter a number");
+                    return;
+                }
+
+                try {
+                    ResultSet rs = database.getStatement().executeQuery("SELECT COUNT(*) as count FROM `book`");
+                    rs.next();
+                    int ID = rs.getInt("count") + 1;
+                    String insert = "INSERT INTO `book`(`id`,`name`,`author`, " +
+                            " `publisher`,`count`) VALUES " +
+                            " ('"+ID+"', '"+name.getText()+"', '"+author.getText()+"', '"+publisher.getText()+"'," +
+                            "'"+num+"');";
+                    database.getStatement().executeUpdate(insert);
+                    JOptionPane.showMessageDialog(frame, "Book Added");
+                    frame.dispose();
+                }
+                catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(frame, ex.getMessage());
+                }
+            }
+        });
+        panel.add(save);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setVisible(true);
+
+    }
+}
--- src/Controller/UpdateBook.java
+++ src/Controller/UpdateBook.java
@@ -1,17 +1,99 @@
-//package Controller;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.*;
-//
-//import Model.Book;
-//import Model.Database;
-//import Model.Operation;
-//import Model.User;
-//
-//public class UpdateBook implements Operation {
-//
-//    @Override
-//    public void operation(Database database, Scanner sc, User user) {
+package Controller;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+import Model.Book;
+import Model.Database;
+import Model.JButton;
+import Model.JLabel;
+import Model.Operation;
+import Model.User;
+
+import javax.swing.*;
+
+public class UpdateBook implements Operation {
+
+    private JTextField name, author, publisher, number;
+    private Database database;
+    private JFrame frame;
+
+    @Override
+    public void operation(Database database, Frame f, User user) {
+
+        this.database = database;
+
+        frame = new JFrame("Update Book");
+        frame.setSize(600,600);
+        frame.setLocationRelativeTo(f);
+        frame.getContentPane().setBackground(new Color(27, 150, 250));
+        frame.setLayout(new BorderLayout());
+
+        Model.JLabel title = new JLabel("Update Book",35);
+        title.setBorder(BorderFactory.createEmptyBorder(20,0,0,0));
+        frame.add(title, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel(new GridLayout(6,2,15,15));
+        panel.setBackground(null);
+        panel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+
+        panel.add(new JLabel("ID: ", 22));
+        String[] ids= new String[] {" "};
+        ArrayList<Integer> idsArray = new ArrayList<>();
+        try{
+            ResultSet rs1 = database.getStatement().executeQuery("SELECT 'ID' FROM `book` ");
+            while (rs1.next()) {
+                idsArray.add(rs1.getInt("ID"));
+            }
+        } catch (Exception e0){
+            JOptionPane.showMessageDialog(frame, e0.getMessage());
+            frame.dispose();
+        };
+
+        ids = new String[idsArray.size() + 1];
+        ids[0] = " ";
+        for (int i = 1; i<= idsArray.size(); i++) {
+            ids[i] = String.valueOf(idsArray.get(i-1));
+        }
+
+        Model.JComboBox id = new Model.JComboBox(ids, 22);
+        id.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateData(id.getSelectedItem().toString());
+            }
+        });
+        panel.add(id);
+
+        panel.add(new JLabel("Name:",22));
+        name = new Model.JTextField(22);
+        panel.add(name);
+
+        panel.add(new JLabel("Author:",22));
+        author = new Model.JTextField(22);
+        panel.add(author);
+
+        panel.add(new JLabel("Publisher:",22));
+        publisher = new Model.JTextField(22);
+        panel.add(publisher);
+
+        panel.add(new JLabel("Number of books:",22));
+        number = new Model.JTextField(22);
+        panel.add(number);
+
+        Model.JButton cancel = new JButton("Cancel",22);
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+
+        });
+        panel.add(cancel);
+
 //        System.out.println("Enter book ID(int):(-1 to show all books)");
 //        int ID = sc.nextInt();
 //        while(ID == -1){
@@ -56,6 +138,13 @@
 //        } catch (SQLException e) {
 //            e.printStackTrace();
 //        }
-//
-//    }
-//}
+
+    }
+    private void updateData(String ID) {
+        if (ID.equals(" ")) {
+            name.setText("");
+            author.setText("");
+            publisher.setText("");
+        }
+    }
+}
