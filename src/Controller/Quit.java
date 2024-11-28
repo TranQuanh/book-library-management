036c3079f8f75eae70db68a51fcbd2030527e10b
Nguyenphuc-dev <nguyenphuc210804@gmail.com>
2024-11-28 21:52:31 +0700

    codeQuit

--- src/Controller/Quit.java
+++ src/Controller/Quit.java
@@ -1,17 +1,18 @@
-//package Controller;
-//
-//
-//import Model.Database;
-//import Model.Operation;
-//import Model.User;
-//
-//import java.util.Scanner;
-//
-//public class Quit implements Operation {
-//
-//    @Override
-//    public void operation(Database database, Scanner sc, User user) {
-//        System.out.println("Thanks for visiting us !");
-//        sc.close();
-//    }
-//}
+package Controller;
+
+import java.util.Scanner;
+
+import Model.Database;
+import Model.Operation;
+import Model.User;
+
+import javax.swing.*;
+
+public class Quit implements Operation {
+    @Override
+    public void operation(Database database, JFrame f, User user) {
+        System.out.println("Thanks for visiting us!");
+
+    }
+}
+
